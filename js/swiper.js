1a16f9c5c6cac54aabd976b3fb23e9e31c25e367
Julio Cesar <juliocesaar.ma@gmail.com>
2024-07-31 13:35:20 -0300

    feat: folders organize

---
+++ js/swiper.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  centeredSlides: true,
+  slidesPerView: 5,
+  //  direction: "horizontal",
+  loop: true,
+  autoplay: {
+    delay: 2000,
+  },
+  speed: 3000, // Velocidade da transição
+  // effect: "fade",
+  // spaceBetween: 100,
+});
---
+++ js/web3modal.js
@@ -0,0 +1,128 @@
+"use strict";
+const Web3Modal = window.Web3Modal.default;
+const WalletConnectProvider = window.WalletConnectProvider.default;
+const EvmChains = window.EvmChains;
+const Fortmatic = window.Fortmatic;
+
+let web3Modal;
+
+let provider;
+
+let selectedAccount;
+
+function init() {
+  console.log("Initializing example");
+
+  // Tell Web3modal what providers we have available.
+  // Built-in web browser provider (only one can exist as a time)
+  // like MetaMask, Brave or Opera is added automatically by Web3modal
+  const providerOptions = {
+    walletconnect: {
+      package: WalletConnectProvider,
+      options: {
+        // Mikko's test key - don't copy as your mileage may vary
+        infuraId: "2527fd9f6cc147be8c74ba8e46528f96",
+      },
+    },
+
+    fortmatic: {
+      package: Fortmatic,
+      options: {
+        // Mikko's TESTNET api key
+        key: "pk_test_391E26A3B43A3350",
+      },
+    },
+  };
+
+  web3Modal = new Web3Modal({
+    cacheProvider: false, // optional
+    providerOptions, // required
+  });
+}
+
+/**
+ * Kick in the UI action after Web3modal dialog has chosen a provider
+ */
+async function fetchAccountData() {
+  // Get a Web3 instance for the wallet
+  const web3 = new Web3(provider);
+
+  console.log("Web3 instance is", web3);
+
+  // Get connected chain id from Ethereum node
+  const chainId = await web3.eth.getChainId();
+
+  if (chainId == 1) {
+    document.querySelector("#network-name").innerHTML = "ETH";
+  } else if (chainId === 56) {
+    document.querySelector("#network-name").innerHTML = "BSC";
+  }
+  // Get average gas price of the connected wallet
+  const gasPrice = await web3.eth.getGasPrice();
+  document.querySelector("#avg-gas-price").innerHTML =
+    web3.utils.fromWei(gasPrice);
+  // Get list of accounts of the connected wallet
+  const accounts = await web3.eth.getAccounts();
+
+  // MetaMask does not give you all accounts, only the selected account
+  console.log("Got accounts", accounts);
+  selectedAccount = accounts[0];
+
+  // Go through all accounts and get their ETH balance
+  const rowResolvers = accounts.map(async (address) => {
+    const balance = await web3.eth.getBalance(address);
+    // ethBalance is a BigNumber instance
+    // https://github.com/indutny/bn.js/
+    const ethBalance = web3.utils.fromWei(balance, "ether");
+    const humanFriendlyBalance = parseFloat(ethBalance).toFixed(4);
+  });
+
+  // Because rendering account does its own RPC commucation
+  // with Ethereum node, we do not want to display any results
+  // until data for all accounts is loaded
+  await Promise.all(rowResolvers);
+}
+
+/**
+ * Fetch account data for UI when
+ * - User switches accounts in wallet
+ * - User switches networks in wallet
+ * - User connects wallet initially
+ */
+async function refreshAccountData() {
+  await fetchAccountData(provider);
+}
+
+/**
+ * Connect wallet
+ */
+async function onConnect() {
+  console.log("Opening a dialog", web3Modal);
+  try {
+    provider = await web3Modal.connect();
+  } catch (e) {
+    console.log("Could not get a wallet connection", e);
+    return;
+  }
+
+  // Subscribe to accounts change
+  provider.on("accountsChanged", (accounts) => {
+    fetchAccountData();
+  });
+
+  // Subscribe to chainId change
+  provider.on("chainChanged", (chainId) => {
+    fetchAccountData();
+  });
+
+  // Subscribe to networkId change
+  provider.on("networkChanged", (networkId) => {
+    fetchAccountData();
+  });
+
+  await refreshAccountData();
+}
+
+window.addEventListener("load", async () => {
+  init();
+});

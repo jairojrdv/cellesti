dd1f21d09daf95d4eebf3a8e3ad740ce88d6bafc
Júlio César <juliocesaar.ma@gmail.com>
2024-10-16 01:29:48 -0300

    feat: add header in mobile

--- js/functions.js
+++ js/functions.js
@@ -205,4 +205,45 @@ $(document).on("click", "#buyButtonAiStartups", function () {
   redirectFunctionBuy()
 });
 
-
+var estado = 0,
+    vw = $(window).width();
+$("#hamburger").click(function() {
+  var delay_time = 0;
+  $(this).toggleClass('open');
+  console.log(estado);
+  if (estado === 0) {
+    TweenMax.to($("#bg-menu-mobile"), 1, {
+      x:-vw,
+      ease: Expo.easeInOut
+    });
+    
+    $("li").each(function() {
+      TweenMax.to($(this), 1.2, {
+        x:-vw,
+        scaleX: 1,
+        delay: delay_time,
+        ease: Expo.easeInOut
+      });
+      delay_time += .04;
+    });
+    estado = 1;
+  } else {
+    estado = 0;
+    TweenMax.to($("#bg-menu-mobile"), 1.2, {
+      x:0,
+      ease: Expo.easeInOut
+    });
+    $("li").each(function() {
+      TweenMax.to($(this), 1, {
+        x:0,
+        /*
+        x:vw,
+        scaleX: 2.3,
+        */
+        delay: delay_time,
+        ease: Expo.easeInOut
+      });
+      delay_time += .02;
+    });
+  }
+});
